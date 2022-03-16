31b2b9ecf40368ed110f8f3e7e666d2dacd60fae
Sidcley <90576535+SidcleyDumont@users.noreply.github.com>
2022-03-16 20:36:51 -0300

    Adição do exercicio 2 Estrutura sequencial

---
+++ Exercicio _Num.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]
+
+a = int(input("Digite o Primeiro Numero: "))
+b = int(input("Digite o Segundo Numero: "))
+
+print("O número informado de (A) é:" , a)
+print("O número informado de (B) é:" , b)
