d077c47844d61b83489454bb10b21d6594d53710
Sidcley <90576535+SidcleyDumont@users.noreply.github.com>
2022-03-16 22:37:40 -0300

    Arquivo Soma

---
+++ Soma.py
@@ -0,0 +1,13 @@
+
+#Programa que peça dois números e imprima a soma
+
+valor1 = int(input("Digite valor 1: "))
+
+valor2 = int(input("Digite valor 2: "))
+
+valor3 = int(input("Digite valor 3: "))
+
+soma = int(valor1) + int(valor2) + int(valor3)
+
+print("O Resultado da soma é:  " , soma)
+
