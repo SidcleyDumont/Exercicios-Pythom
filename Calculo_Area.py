2f466d282064017236c23f36ccbf4dee372ced58
Sidcley <90576535+SidcleyDumont@users.noreply.github.com>
2022-03-19 00:15:17 -0300

    Calculo Area

---
+++ Calculo_Area.py
@@ -0,0 +1,12 @@
+#Programa que peça o raio de um círculo, calcule e mostre sua área
+
+from socket import SOMAXCONN
+
+
+raio = float(input("Digite Raio do Circulo: "))
+
+area = float(input("Digite Area: "))
+
+soma = raio + area
+
+print("Valor da Area é:" , soma)
