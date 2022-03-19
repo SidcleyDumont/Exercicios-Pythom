eb73c0b7c4d91dbb575279b2544ef6a95d946f3b
Sidcley <90576535+SidcleyDumont@users.noreply.github.com>
2022-03-19 00:08:45 -0300

    Converter Metros em Centimetros

---
+++ Converção_Metros_Centimetros.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros
+
+metros = float (input("Digite Valor em Metros: "))
+
+centimetros = metros * 100;
+
+print("Valor convertido é: " , centimetros)
